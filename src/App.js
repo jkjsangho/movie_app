65d6f9a96fb43ca6104b18952fe486c37eea99c7
hoho <jkjsangho@naver.com>
2020-10-06 17:52:34 +0900

    test v0.1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+function Food(props){
+  console.log(props);
+  return <h1>Potata?!</h1>
+}
+
 function App() {
-  return (<div>Hello</div>
+  return (<div>Hello
+              <Food 
+                fav="kimchi"
+                somthing={true}
+                papa={["potato123",1,2,3,4,true]}
+             />
+          </div>
   );
 }
 
