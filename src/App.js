57780380ca72e0caaba40f62eb20f40bcdd31dce
hoho <jkjsangho@naver.com>
2020-10-08 09:38:47 +0900

    test v0.2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React from 'react';
+//import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./movie"
 
-function Food(props){
-  console.log(props);
-  return <h1>Potata?!</h1>
-}
 
-function App() {
-  return (<div>Hello
-              <Food 
-                fav="kimchi"
-                somthing={true}
-                papa={["potato123",1,2,3,4,true]}
-             />
-          </div>
-  );
+
+class App extends React.Component{
+
+  state ={
+    isLoading: true,
+    movies: []
+
+  }
+
+  getMovies = async() =>{
+    const {data:{data:{movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    //const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState({movies, isLoading:false})
+  }
+   componentDidMount(){
+    /*
+    setTimeout(()=>{
+      this.setState({isLoading: false})}
+      ,6000);
+    */
+    this.getMovies();
+  }
+
+  render(){
+    const{isLoading, movies} = this.state;
+    return <div>
+            {isLoading ? "Loading..." : movies.map(movie=>(
+            <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
+            )
+          )}
+
+    </div>
+  }
 }
 
 export default App;
