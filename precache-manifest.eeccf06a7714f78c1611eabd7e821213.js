adb7626f9089a9037e4138ebfe626be625324874
hoho <jkjsangho@naver.com>
2020-10-12 11:29:22 +0900

    Updates

---
+++ precache-manifest.eeccf06a7714f78c1611eabd7e821213.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdbe7fa236f9f9e849e0f692e8f24fe8",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "2ee167a55be76b1841a3",
+    "url": "/movie_app/static/css/main.2c35d1bc.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ee167a55be76b1841a3",
+    "url": "/movie_app/static/js/main.85759506.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
